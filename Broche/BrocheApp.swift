ea159b9d75157048717a8e092bd8901684c98cea
Jacob Johnson <jjskunk0503@gmail.com>
2023-05-18 13:26:08 -0400

    Initial Commit

---
+++ Broche/BrocheApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrocheApp.swift
+//  Broche
+//
+//  Created by Jacob Johnson on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct BrocheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
