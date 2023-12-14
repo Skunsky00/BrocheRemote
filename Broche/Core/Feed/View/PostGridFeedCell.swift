ebf1422205bb9680e958ecd049798b5971ffb90b
Jacob Johnson <jjskunk0503@gmail.com>
2023-12-13 21:40:09 -0500

    comments, postgrid, feedview scroll like tiktok, location for feed, feedcell ui changes

---
+++ Broche/Core/Feed/View/PostGridFeedCell.swift
@@ -0,0 +1,18 @@
+//
+//  PostGridFeedCell.swift
+//  Broche
+//
+//  Created by Jacob Johnson on 12/13/23.
+//
+
+import SwiftUI
+
+struct PostGridFeedCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostGridFeedCell()
+}
