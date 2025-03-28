c9164ff4c3d9f8e6fd2e84071003261c6eefe795
Jacob Johnson <jjskunk0503@gmail.com>
2025-03-27 20:31:38 -0400

    added the pinned tab

---
+++ Broche/Core/Components/View/BrocheGridView.swift
@@ -0,0 +1,18 @@
+//
+//  BrocheGridView.swift
+//  Broche
+//
+//  Created by Jacob Johnson on 3/25/25.
+//
+
+import SwiftUI
+
+struct BrocheGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BrocheGridView()
+}
---
+++ Broche/Core/Components/ViewModel/BrocheGridViewModal.swift
@@ -0,0 +1,8 @@
+//
+//  BrocheGridViewModal.swift
+//  Broche
+//
+//  Created by Jacob Johnson on 3/25/25.
+//
+
+import Foundation
